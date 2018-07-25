fce0be3065ff8710e0e037a7f2d151e93864c06d
Tobias Drewry <drewry@mesamundi.com>
2018-07-25 16:54:20 -0400

    conversation manager work-in-progress -- non-functional

--- spigot-framework/src/main/java/com/enjin/enjincoin/spigot_framework/controllers/ConversationManager.java
+++ spigot-framework/src/main/java/com/enjin/enjincoin/spigot_framework/controllers/ConversationManager.java
@@ -1,16 +1,23 @@
 package com.enjin.enjincoin.spigot_framework.controllers;
 
 import com.enjin.enjincoin.spigot_framework.BasePlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.conversations.Conversation;
 import org.bukkit.conversations.ConversationAbandonedEvent;
-import org.bukkit.conversations.ConversationAbandonedListener;
 import org.bukkit.conversations.ConversationFactory;
+import org.bukkit.conversations.ManuallyAbandonedConversationCanceller;
 
-public class ConversationManager implements ConversationAbandonedListener {
+import java.util.LinkedList;
+import java.util.logging.Level;
+
+public class ConversationManager {
 
     /**
      * <p>The spigot plugin.</p>
      */
     private ConversationFactory conversationFactory;
+
+    private LinkedList<Conversation> conversationQueue;
 
     /**
      * <p>The spigot plugin.</p>
@@ -19,8 +26,58 @@ public class ConversationManager implements ConversationAbandonedListener {
 
     public ConversationManager(BasePlugin main) { this.main = main; }
 
-    @Override
-    public void conversationAbandoned(ConversationAbandonedEvent conversationAbandonedEvent) {
+    public LinkedList<Conversation> accessConversationQueue() { return this.conversationQueue; }
 
+    public synchronized boolean beginConversation(Conversation conversation) {
+        if (!conversationQueue.contains(conversation)) {
+            conversationQueue.addLast(conversation);
+            if (conversationQueue.getFirst() == conversation) {
+                conversation.begin();
+                conversation.outputNextPrompt();
+                return true;
+            }
+        }
+        return true;
+    }
+
+    public synchronized  void abandonConversation(Conversation conversation, ConversationAbandonedEvent details) {
+        if (!conversationQueue.isEmpty()) {
+            if (conversationQueue.getFirst() == conversation) {
+                conversation.abandon(details);
+            }
+            if (conversationQueue.contains(conversation)) {
+                conversationQueue.remove(conversation);
+            }
+            if (!conversationQueue.isEmpty()) {
+                conversationQueue.getFirst().outputNextPrompt();
+            }
+        }
+    }
+
+    public synchronized  void abandonAllConversations() {
+
+        LinkedList<Conversation> oldQueue = conversationQueue;
+        conversationQueue = new LinkedList<Conversation>();
+        for (Conversation conversation : oldQueue) {
+            try {
+                conversation.abandon(new ConversationAbandonedEvent(conversation, new ManuallyAbandonedConversationCanceller()));
+            } catch (Throwable t) {
+                Bukkit.getLogger().log(Level.SEVERE, "Unexpected exception while abandoning a conversation", t);
+            }
+        }
+    }
+
+    public synchronized  void acceptConversationInput(String input) {
+        if (isConversing()) {
+            conversationQueue.getFirst().acceptInput(input);
+        }
+    }
+
+    public synchronized  boolean isConversing() {
+        return !conversationQueue.isEmpty();
+    }
+
+    public synchronized  boolean isConversingModaly() {
+        return isConversing() && conversationQueue.getFirst().isModal();
     }
 }
