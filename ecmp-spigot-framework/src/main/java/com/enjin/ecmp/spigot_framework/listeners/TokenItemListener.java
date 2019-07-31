a4a82dd5e751a902de048ba7c3f22ce9f88cefea
Evan Lindsay <evanlindsay@live.com>
2019-07-30 22:15:54 -0700

    EDT-3332 Prevent using tokenized items in crafting

--- ecmp-spigot-framework/src/main/java/com/enjin/ecmp/spigot_framework/listeners/TokenItemListener.java
+++ ecmp-spigot-framework/src/main/java/com/enjin/ecmp/spigot_framework/listeners/TokenItemListener.java
@@ -6,6 +6,7 @@ import com.enjin.ecmp.spigot_framework.util.TokenUtils;
 import com.enjin.ecmp.spigot_framework.wallet.MutableBalance;
 import com.enjin.ecmp.spigot_framework.wallet.TokenWallet;
 import com.enjin.ecmp.spigot_framework.wallet.TokenWalletView;
+import com.enjin.java_commons.StringUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
@@ -13,8 +14,10 @@ import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.inventory.CraftingInventory;
 import org.bukkit.inventory.InventoryView;
 import org.bukkit.inventory.ItemStack;
 
@@ -51,6 +54,19 @@ public class TokenItemListener implements Listener {
         Item item = event.getItemDrop();
         String id = TokenUtils.getTokenID(item.getItemStack());
         event.setCancelled(id != null);
+    }
+
+    @EventHandler
+    public void onCraftItem(CraftItemEvent event) {
+        CraftingInventory inventory = event.getInventory();
+        for (int i = inventory.getSize() - 1; i >= 0; i--) {
+            ItemStack is = inventory.getItem(i);
+            String id = TokenUtils.getTokenID(is);
+            if (!StringUtils.isEmpty(id)) {
+                event.setCancelled(true);
+                return;
+            }
+        }
     }
 
 }
