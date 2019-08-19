327bb81bb6b54ee6996c88621df9869cbb07b28f
Evan Lindsay <evanlindsay@live.com>
2019-08-19 11:55:37 -0700

    EDT-3376 Tab completion of sub-commands, arguments not supported yet

--- spigot/src/main/java/com/enjin/ecmp/spigot/cmd/CmdEnj.java
+++ spigot/src/main/java/com/enjin/ecmp/spigot/cmd/CmdEnj.java
@@ -5,11 +5,13 @@ import com.enjin.ecmp.spigot.player.UnregisteredPlayerException;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
-public class CmdEnj extends EnjCommand implements CommandExecutor {
+public class CmdEnj extends EnjCommand implements CommandExecutor, TabCompleter {
 
     private CmdBalance cmdBalance;
     private CmdHelp cmdHelp;
@@ -21,6 +23,7 @@ public class CmdEnj extends EnjCommand implements CommandExecutor {
 
     public CmdEnj(SpigotBootstrap bootstrap) {
         super(bootstrap);
+        this.aliases.add("enj");
         this.addSubCommand(cmdBalance = new CmdBalance(bootstrap));
         this.addSubCommand(cmdHelp = new CmdHelp(bootstrap));
         this.addSubCommand(cmdLink = new CmdLink(bootstrap));
@@ -33,7 +36,7 @@ public class CmdEnj extends EnjCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         try {
-            process(new CommandContext(sender, new ArrayList<>(Arrays.asList(args)), label));
+            process(new CommandContext(sender, new ArrayList<>(Arrays.asList(args)), label), true);
         } catch (UnregisteredPlayerException ex) {
             ex.printStackTrace();
         }
@@ -47,4 +50,10 @@ public class CmdEnj extends EnjCommand implements CommandExecutor {
             cmdHelp.execute(context);
     }
 
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        CommandContext context = new CommandContext(sender, new ArrayList<>(Arrays.asList(args)), label);
+        process(context, false);
+        return context.tabCompletionResult;
+    }
 }
