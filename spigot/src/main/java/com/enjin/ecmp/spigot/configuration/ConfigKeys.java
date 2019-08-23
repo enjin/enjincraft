bdcd9a355b0ca2b537b3242d7cedb944afda300b
Evan Lindsay <evanlindsay@live.com>
2019-08-23 16:37:16 -0700

    EDT-3375 Regular items no longer supported in trading

--- spigot/src/main/java/com/enjin/ecmp/spigot/configuration/ConfigKeys.java
+++ spigot/src/main/java/com/enjin/ecmp/spigot/configuration/ConfigKeys.java
@@ -6,7 +6,6 @@ public final class ConfigKeys {
     public static final String APP_ID = "appId";
     public static final String APP_SECRET = "secret";
     public static final String DEV_IDENT_ID = "devIdentityId";
-    public static final String VANILLA_ITEMS_TRADING = "allowVanillaItemsInTrades";
     public static final String DEBUGGING = "debugging";
     public static final String SDK_DEBUG = "sdk";
     public static final String PLUGIN_DEBUG = "plugin";
