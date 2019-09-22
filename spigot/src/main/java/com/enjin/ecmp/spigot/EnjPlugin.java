1c15c2515c11199539d882255d5646b55c0ecd82
Evan Lindsay <evanlindsay@live.com>
2019-09-22 12:58:47 -0700

    EDT-3468 Implement support for runtime dependency downloading and loading

--- spigot/src/main/java/com/enjin/ecmp/spigot/EnjPlugin.java
+++ spigot/src/main/java/com/enjin/ecmp/spigot/EnjPlugin.java
@@ -1,13 +1,27 @@
 package com.enjin.ecmp.spigot;
 
+import com.enjin.ecmp.spigot.dependencies.DependencyConfig;
+import com.enjin.ecmp.spigot.dependencies.DependencyManager;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class EnjPlugin extends JavaPlugin {
 
+    private DependencyManager dependencyManager;
     private SpigotBootstrap bootstrap;
 
     @Override
     public void onEnable() {
+        dependencyManager = new DependencyManager(this);
+        YamlConfiguration dependencyYamlConfig = loadYamlResource("dependencies.yml");
+        DependencyConfig dependencyConfig = DependencyConfig.create(dependencyYamlConfig);
+        dependencyManager.loadDependencies(dependencyConfig);
+
         ECMP.register(bootstrap = new SpigotBootstrap(this));
         bootstrap.setUp();
     }
@@ -16,6 +30,19 @@ public class EnjPlugin extends JavaPlugin {
     public void onDisable() {
         bootstrap.tearDown();
         ECMP.unregister();
+    }
+
+    public YamlConfiguration loadYamlResource(String resource, Charset charset) {
+        InputStream is = getResource(resource);
+
+        if (is == null)
+            return null;
+
+        return YamlConfiguration.loadConfiguration(new InputStreamReader(is, charset));
+    }
+
+    public YamlConfiguration loadYamlResource(String resource) {
+        return loadYamlResource(resource, StandardCharsets.UTF_8);
     }
 
     public SpigotBootstrap bootstrap() {
