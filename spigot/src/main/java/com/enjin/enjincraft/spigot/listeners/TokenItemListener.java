94d6861ff558563c6c5e1e7cd77f3c3f35d7a8cb
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2020-04-22 17:39:44 -0700

    - ESP-31 Begun implementing fixes to prevent in-game token loss

--- spigot/src/main/java/com/enjin/enjincraft/spigot/listeners/TokenItemListener.java
+++ spigot/src/main/java/com/enjin/enjincraft/spigot/listeners/TokenItemListener.java
@@ -6,17 +6,21 @@ import com.enjin.enjincraft.spigot.util.StringUtils;
 import com.enjin.enjincraft.spigot.util.TokenUtils;
 import com.enjin.enjincraft.spigot.wallet.MutableBalance;
 import com.enjin.enjincraft.spigot.wallet.TokenWallet;
-import org.bukkit.entity.Item;
+import org.bukkit.Material;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.event.inventory.*;
+import org.bukkit.event.player.PlayerArmorStandManipulateEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
-import org.bukkit.inventory.CraftingInventory;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class TokenItemListener implements Listener {
 
@@ -62,6 +66,85 @@ public class TokenItemListener implements Listener {
     }
 
     @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        ItemStack clk = event.getCurrentItem();
+        ItemStack held = event.getCursor();
+
+        if (held != null && held.getType() != Material.AIR)
+            InventoryClickHolding(event, held);
+        else if (clk != null && clk.getType() != Material.AIR)
+            InventoryClickClicked(event, clk);
+    }
+
+    private void InventoryClickClicked(InventoryClickEvent event, ItemStack is) {
+        String tokenId = TokenUtils.getTokenID(is);
+
+        if (StringUtils.isEmpty(tokenId))
+            return;
+
+        Inventory clkInv = event.getClickedInventory();
+
+        /* TODO: Ought to check if the token belongs to the player and if not
+         *       is the player linked?
+         */
+
+        if (!(clkInv instanceof PlayerInventory) || event.getAction() != InventoryAction.MOVE_TO_OTHER_INVENTORY)
+            return;
+
+        event.setCancelled(true);
+    }
+
+    private void InventoryClickHolding(InventoryClickEvent event, ItemStack is) {
+        String tokenId = TokenUtils.getTokenID(is);
+
+        if (StringUtils.isEmpty(tokenId))
+            return;
+
+        Inventory clkInv = event.getClickedInventory();
+
+        if (clkInv instanceof PlayerInventory)
+            return;
+
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onInventoryDrag(InventoryDragEvent event) {
+        Inventory top = event.getView().getTopInventory();
+        Inventory bottom = event.getView().getBottomInventory();
+
+        int playerUpper;
+        int playerLower;
+        if (top instanceof PlayerInventory == bottom instanceof PlayerInventory)  {
+            return;
+        } else if (top instanceof PlayerInventory) {
+            playerUpper = top.getSize();
+            playerLower = 0;
+        } else {
+            playerUpper = bottom.getSize() + top.getSize();
+            playerLower = top.getSize();
+        }
+
+        AtomicBoolean otherModified = new AtomicBoolean(false); // Assume false
+        Map<Integer, ItemStack> newItems = event.getNewItems();
+
+        // Checks if a token was placed outside the player's inventory
+        newItems.forEach((rawSlot, is) -> {
+            if (is == null || is.getType() == Material.AIR)
+                return;
+
+            String tokenId = TokenUtils.getTokenID(is);
+
+            // TODO: Ought to check if the token belongs to the player
+
+            if (!StringUtils.isEmpty(tokenId) && (rawSlot < playerLower || rawSlot >= playerUpper))
+                otherModified.set(true);
+        });
+
+        event.setCancelled(otherModified.get());
+    }
+
+    @EventHandler
     public void onCraftItem(CraftItemEvent event) {
         CraftingInventory inventory = event.getInventory();
         for (int i = inventory.getSize() - 1; i >= 0; i--) {
@@ -74,6 +157,43 @@ public class TokenItemListener implements Listener {
             event.setCancelled(true);
             break;
         }
+    }
+
+    @EventHandler
+    public void onArmorStandInteract(PlayerArmorStandManipulateEvent event) {
+        ItemStack held = event.getPlayerItem();
+
+        if (held.getType() == Material.AIR || event.getArmorStandItem().getType() != Material.AIR)
+            return;
+
+        String tokenId = TokenUtils.getTokenID(held);
+
+        if (StringUtils.isEmpty(tokenId))
+            return;
+
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onInteractEntity(PlayerInteractEntityEvent event) {
+        Entity entity = event.getRightClicked();
+
+        if (entity instanceof ItemFrame)
+            InteractItemFrame(event, (ItemFrame) entity);
+    }
+
+    private void InteractItemFrame(PlayerInteractEntityEvent event, ItemFrame itemFrame) {
+        ItemStack held = event.getPlayer().getInventory().getItemInMainHand();
+
+        if (held.getType() == Material.AIR || itemFrame.getItem().getType() != Material.AIR)
+            return;
+
+        String tokenId = TokenUtils.getTokenID(held);
+
+        if (StringUtils.isEmpty(tokenId))
+            return;
+
+        event.setCancelled(true);
     }
 
 }
