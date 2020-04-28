c9bb1039c247608d0d4fe743c7acf558ad56f1b0
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2020-04-28 12:29:35 -0700

    - ESP-35 Refactored event listener for new EnjinCloud events

--- spigot/src/main/java/com/enjin/enjincraft/spigot/listeners/EnjEventListener.java
+++ spigot/src/main/java/com/enjin/enjincraft/spigot/listeners/EnjEventListener.java
@@ -6,7 +6,9 @@ import com.enjin.enjincraft.spigot.player.EnjPlayer;
 import com.enjin.enjincraft.spigot.wallet.MutableBalance;
 import com.enjin.sdk.models.notification.*;
 import com.enjin.sdk.models.request.TransactionType;
+import com.google.gson.JsonObject;
 import org.bukkit.Bukkit;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
 import java.util.Optional;
@@ -23,28 +25,31 @@ public class EnjEventListener implements com.enjin.sdk.services.notification.Not
     @Override
     public void notificationReceived(NotificationEvent event) {
         try {
-            bootstrap.debug(event.getData());
-            NotificationType eventType = event.getType();
+            bootstrap.debug(String.format("Received event: %s", event));
+            EventType eventType = event.getType();
 
             if (eventType == null)
                 return;
 
             switch (eventType) {
-                case TX_EXECUTED:
-                    onTxExecuted(event.getEvent());
+                case TRADE_COMPLETED:
+                case TRADE_CREATED:
+                    onTradeExecuted(event);
                     break;
-                case TXR_CANCELED_USER:
-                case TXR_CANCELED_PLATFORM:
-                    onTxrCancelled(event.getEvent());
+                case TRANSACTION_CANCELED:
+                    onTxCancelled(event);
                     break;
                 case IDENTITY_LINKED:
-                    onIdentityLinked(event.getEvent());
+                    onIdentityLinked(event);
                     break;
-                case IDENTITY_UPDATED:
-                    onIdentityUpdated(event.getEvent());
+                case IDENTITY_DELETED:
+                case IDENTITY_UNLINKED:
+                    onIdentityUnlinked(event);
                     break;
-                case BALANCE_UPDATED:
-                    onBalanceUpdated(event.getEvent());
+                case TOKEN_MELTED:
+                case TOKEN_MINTED:
+                case TOKEN_TRANSFERRED:
+                    onTokenUpdated(event);
                     break;
                 default:
                     break;
@@ -54,78 +59,201 @@ public class EnjEventListener implements com.enjin.sdk.services.notification.Not
         }
     }
 
-    private void onTxExecuted(Event event) {
-        EventData data = event.getData();
-        TransactionType type = data.getRequestType();
-
-        switch (type) {
-            case CREATE_TRADE:
-                onCreateTrade(data);
-                break;
-            case COMPLETE_TRADE:
-                onCompleteTrade(data);
-                break;
-            default:
-                break;
-        }
-    }
-
-    private void onTxrCancelled(Event event) {
-        EventData data = event.getData();
-        TransactionType type = data.getRequestType();
-
-        switch (type) {
-            case CREATE_TRADE:
-            case COMPLETE_TRADE:
-                cancelTrade(data);
-                break;
-            default:
-                break;
-        }
-    }
-
-    private void onIdentityLinked(Event event) {
-        EventData data = event.getData();
-        if (data.getId() == null)
+    private void onTradeExecuted(NotificationEvent event) {
+        if (!event.getChannel().contains("app"))
             return;
 
-        EnjPlayer enjPlayer = bootstrap.getPlayerManager().getPlayer(data.getId()).orElse(null);
+        TransactionType type = getTransactionType(event);
+
+        if (type == null)
+            return;
+
+        int transactionId;
+        String tradeId;
+
+        try {
+            JsonObject transaction = event.getEventData()
+                    .get("transaction")
+                    .getAsJsonObject();
+            JsonObject trade = event.getEventData()
+                    .get("trade")
+                    .getAsJsonObject();
+            transactionId = transaction.get("id").getAsInt();
+            tradeId = trade.get("id").getAsString();
+        } catch (Exception e) {
+            bootstrap.log(e);
+            return;
+        }
+
+        switch (type) {
+            case CREATE_TRADE:
+                bootstrap.getTradeManager().sendCompleteRequest(transactionId, tradeId);
+                break;
+            case SEND:
+                bootstrap.getTradeManager().completeTrade(transactionId);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void onTxCancelled(NotificationEvent event) {
+        if (!event.getChannel().contains("app"))
+            return;
+
+        TransactionType type = getTransactionType(event);
+
+        if (type == null)
+            return;
+
+        int transactionId;
+
+        try {
+            JsonObject transaction = event.getEventData()
+                    .get("transaction")
+                    .getAsJsonObject();
+            transactionId = transaction.get("id").getAsInt();
+        } catch (Exception e) {
+            bootstrap.log(e);
+            return;
+        }
+
+        switch (type) {
+            case CREATE_TRADE:
+            case COMPLETE_TRADE:
+                bootstrap.getTradeManager().cancelTrade(transactionId);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void onIdentityLinked(NotificationEvent event) {
+        if (!event.getChannel().contains("identity"))
+            return;
+
+        JsonObject identity = event.getEventData().getAsJsonObject("identity");
+
+        if (identity == null || identity.get("id") == null)
+            return;
+
+        int id = identity.get("id").getAsInt();
+
+        EnjPlayer enjPlayer = bootstrap.getPlayerManager().getPlayer(id).orElse(null);
         if (enjPlayer == null)
             return;
 
         Bukkit.getScheduler().runTaskAsynchronously(bootstrap.plugin(), enjPlayer::reloadIdentity);
     }
 
-    private void onIdentityUpdated(Event event) {
-        EventData data = event.getData();
-        if (data.getId() == null)
+    private void onIdentityUnlinked(NotificationEvent event) {
+        if (!event.getChannel().contains("identity"))
             return;
 
-        Optional<EnjPlayer> playerOptional = bootstrap.getPlayerManager().getPlayer(data.getId());
+        JsonObject identity = event.getEventData().getAsJsonObject("identity");
+
+        if (identity == null || identity.get("id") == null)
+            return;
+
+        int id = identity.get("id").getAsInt();
+
+        Optional<EnjPlayer> playerOptional = bootstrap.getPlayerManager().getPlayer(id);
         playerOptional.ifPresent(player -> {
-            if (data.getParam1() == null || data.getParam1().isEmpty())
-                return;
             Bukkit.getScheduler().runTaskAsynchronously(bootstrap.plugin(), player::unlinked);
         });
     }
 
-    private void onBalanceUpdated(Event event) {
-        String balanceRaw = event.getData().getParam4();
-        String ethAddr = event.getData().getParam1();
-        String tokenId = event.getData().getParam2();
-        // Sets balance to zero if parameter is empty
-        Integer balance = balanceRaw.isEmpty() ? Integer.valueOf(0) : Integer.parseInt(event.getData().getParam4());
+    private void onTokenUpdated(NotificationEvent event) {
+        if (!event.getChannel().contains("app"))
+            return;
 
+        TransactionType type = getTransactionType(event);
+
+        if (type == null)
+            return;
+
+        switch (type) {
+            case MELT:
+            case MINT:
+            case SEND:
+                updateBalance(type, event.getEventData());
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void updateBalance(TransactionType type, JsonObject data) {
+        String tokenId;
+        int amount;
+
+        try {
+            tokenId = data.get("token")
+                    .getAsJsonObject()
+                    .get("id")
+                    .getAsString();
+        } catch (Exception e) {
+            bootstrap.log(e);
+            return;
+        }
+
+        if (type == TransactionType.SEND) {
+            String from;
+            String to;
+
+            try {
+                JsonObject transfer = data.get("transfer").getAsJsonObject();
+                from = transfer.get("from").getAsString();
+                to = transfer.get("to").getAsString();
+                amount = transfer.get("value").getAsInt();
+            } catch (Exception e) {
+                bootstrap.log(e);
+                return;
+            }
+
+            updateBalance(from, tokenId, -amount);
+            updateBalance(to, tokenId, amount);
+        } else if (type == TransactionType.MELT) {
+            String ethAddr;
+
+            try {
+                JsonObject melt = data.get("melt").getAsJsonObject();
+                ethAddr = melt.get("owner").getAsString();
+                amount = melt.get("value").getAsInt();
+            } catch (Exception e) {
+                bootstrap.log(e);
+                return;
+            }
+
+            updateBalance(ethAddr, tokenId, -amount);
+        } else if (type == TransactionType.MINT) {
+            String ethAddr;
+
+            try {
+                JsonObject mint = data.get("mint").getAsJsonObject();
+                JsonObject transaction = data.get("transaction").getAsJsonObject();
+                ethAddr = mint.get("to").getAsString();
+                amount = transaction.get("value").getAsInt();
+            } catch (Exception e) {
+                bootstrap.log(e);
+                return;
+            }
+
+            updateBalance(ethAddr, tokenId, amount);
+        }
+    }
+
+    private void updateBalance(String ethAddr, String tokenId, int balanceDelta) {
         EnjPlayer enjPlayer = bootstrap.getPlayerManager().getPlayer(ethAddr).orElse(null);
-        TokenModel tokenModel = bootstrap.getTokenManager().getToken(tokenId);
 
         if (enjPlayer == null || enjPlayer.getTokenWallet() == null)
             return;
 
+        TokenModel tokenModel = bootstrap.getTokenManager().getToken(tokenId);
         MutableBalance mBalance = enjPlayer.getTokenWallet().getBalance(tokenId);
 
-        if (mBalance == null && balance > 0) {
-            mBalance = new MutableBalance(tokenId, event.getData().getParam3(), balance);
+        if (mBalance == null && balanceDelta > 0) {
+            mBalance = new MutableBalance(tokenId, null, balanceDelta);
             enjPlayer.getTokenWallet().setBalance(mBalance);
 
             // Adds the token's permissions to the player
@@ -136,6 +264,8 @@ public class EnjEventListener implements com.enjin.sdk.services.notification.Not
                 perms.forEach(perm -> { enjPlayer.addPermission(perm, tokenId, world); });
             }
         } else if (mBalance != null) {
+            int balance = mBalance.balance() + balanceDelta;
+
             if (balance > 0) {
                 mBalance.set(balance);
             } else {
@@ -154,23 +284,27 @@ public class EnjEventListener implements com.enjin.sdk.services.notification.Not
         enjPlayer.validateInventory();
     }
 
-    private void onCreateTrade(EventData data) {
-        if (data.getId() == null)
-            return;
-        String tradeId = data.getParam1();
-        bootstrap.getTradeManager().sendCompleteRequest(data.getId(), tradeId);
-    }
+    private TransactionType getTransactionType(NotificationEvent event) {
+        String typeString;
 
-    private void onCompleteTrade(EventData data) {
-        if (data.getId() == null)
-            return;
-        bootstrap.getTradeManager().completeTrade(data.getId());
-    }
+        try {
+            JsonObject transaction = event.getEventData()
+                    .get("transaction")
+                    .getAsJsonObject();
+            typeString = transaction.get("type").getAsString();
+        } catch (Exception e) {
+            bootstrap.log(e);
+            return null;
+        }
 
-    private void cancelTrade(EventData data) {
-        if (data.getId() == null)
-            return;
-        bootstrap.getTradeManager().cancelTrade(data.getId());
+        for (TransactionType transactionType : TransactionType.values()) {
+            if (typeString.equalsIgnoreCase(transactionType.name()))
+                return transactionType;
+        }
+
+        bootstrap.debug(String.format("No such transaction type: %s", typeString));
+
+        return null;
     }
 
 }
