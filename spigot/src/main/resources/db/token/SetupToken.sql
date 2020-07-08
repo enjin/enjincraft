4700aba1810d97840cb8f7cdd2fdac9747caaf48
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2020-07-07 18:05:06 -0700

    - Added UpdateTokenInstance SQL statement - Removed GetNBT SQL statement - Removed UpdateNBT SQL statement

--- spigot/src/main/resources/db/token/SetupToken.sql
+++ spigot/src/main/resources/db/token/SetupToken.sql
@@ -3,7 +3,6 @@ CREATE TABLE IF NOT EXISTS token
     "token_id"          VARCHAR(16) NOT NULL,
     "nonfungible"       BOOLEAN NOT NULL DEFAULT 0,
     "alternate_id"      TEXT NULLABLE,
-    "metadata_uri"      TEXT NULLABLE,
     "wallet_view_state" TEXT NULLABLE,
 
     CHECK ("nonfungible" IN (0,1)),
